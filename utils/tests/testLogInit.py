f9c6471e22ca410a73c92c5fe18dc88b8eedd267
goffer123 <goffersoft@gmail.com>
2014-06-11 12:00:03 -0400

    pyton2/3 coexistence

--- utils/tests/testLogInit.py
+++ utils/tests/testLogInit.py
@@ -14,12 +14,20 @@ class TestLogInit(TestCase):
             logger.debug('a debug message')
             logger.critical('a critical message')
             logger.warn('a warning message')
-            logger.exception('a exception message')
+            exc_msg = 'the if statement throws an exception'\
+                      'in the python3 world(as it should)'\
+                      'but gets executed in the python2 world'
+            try:
+                if('hello' > 3):
+                    logger.exception(exc_msg)
+            except:
+                logger.exception(exc_msg)
+
             l.check(
                 ('root', 'INFO', 'a info message'),
                 ('root', 'ERROR', 'a error message'),
                 ('root', 'DEBUG', 'a debug message'),
                 ('root', 'CRITICAL', 'a critical message'),
                 ('root', 'WARNING', 'a warning message'),
-                ('root', 'ERROR', 'a exception message'),
+                ('root', 'ERROR', exc_msg),
                 )
